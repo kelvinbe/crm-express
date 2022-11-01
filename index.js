73f6bf39ce754a4d9bac257d96b992b2f4ecbfdc
kelvin beno <kelvinbeno526@gmail.com>
2022-11-01 13:47:21 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import routes from "./src/routes/crmRoutes";
+
+
+
+
+const app = express();
+routes(app)
+
+const db =
+  "mongodb+srv://Kelvin:1234@cluster0.8no4u.mongodb.net/gaming?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on port:${PORT}`));
+  })
+  .catch((err) => console.error(err));
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use(express.json())
+
+app.get('/', (req, res) => {
+
+    res.send(`Node is running smoothly`)
+})
+
+
